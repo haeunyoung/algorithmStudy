aed2392000797611735436e144431ca926fa56bb
haeunyoung <44631215+haeunyoung@users.noreply.github.com>
2020-06-09 04:59:07 +0900

    day43

--- algorithm/algorithm/day43_4963.cpp
+++ algorithm/algorithm/day43_4963.cpp
@@ -1,14 +1,40 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+vector<pair<int, int>> tree[55][55];
+int c[55][55];
+int c_index;
+void dfs(int x, int y)
+{
+	if (c[x][y] != 0)return;
+	c[x][y] = c_index;
 
+	for (int i = 0; i < tree[x][y].size(); i++)
+	{
+		int a = tree[x][y][i].first;
+		int b = tree[x][y][i].second;
+		dfs(a, b);
+	}
+}
 int main(void)
 {
-	int w,h;
+	int w, h;
 
 	while (1)
 	{
-		int arr[51][51] = {0,};
+		c_index = 1;
+		for (int i = 0; i < 55; i++)
+		{
+			for (int j = 0; j < 55; j++)
+			{
+				tree[i][j].clear();
+				c[i][j] = 0;
+
+			}
+		}
+
+
+		int arr[51][51] = { 0, };
 		cin >> w >> h;
 		if (w == 0 && h == 0)break;
 		vector<pair<int, int>> v;
@@ -20,77 +46,46 @@ int main(void)
 				scanf("%d", &arr[i][j]);
 				if (arr[i][j] == 1)
 				{
-					v.push_back(pair<int,int>(i, j));
+					v.push_back(pair<int, int>(i, j));
 				}
 			}
 		}
-		int land_index = 1;
-		int land[51][51] = { 0, };
+
 		for (int i = 0; i < v.size(); i++)
 		{
 			int x = v[i].first, y = v[i].second;
-			if (land[x][y] == 0)
-			{
-				for (int a = -1; a < 2; a++)
-				{
-					if (x + a < 0)continue;
-					if (x + a >= h)continue;
-					for (int b = -1; b < 2; b++)
-					{
-						if (y + b < 0)continue;
-						if (y + b >= w)continue;
 
-						if (land[x + a][y + b] != 0)
-						{
-							land[x][y] = land[x + a][y + b];
-							a = 2;
-							b = 2;
-						}
+			for (int a = -1; a < 2; a++)
+			{
+				if (x + a < 0)continue;
+				if (x + a >= h)continue;
+				for (int b = -1; b < 2; b++)
+				{
+					if (y + b < 0)continue;
+					if (y + b >= w)continue;
+
+					if (arr[x + a][y + b] == 1)
+					{
+						tree[x][y].push_back(pair<int, int>(x + a, y + b));
+						tree[x + a][y + b].push_back(pair<int, int>(x, y));
 					}
 				}
-				if (land[x][y] == 0)
-				{
-					land[x][y] = land_index;
-					land_index++;
-				}
-
 			}
+
 		}
-		for (int i = v.size()-1; i >=0; i--)
+
+		for (int i = 0; i < v.size(); i++)
 		{
 			int x = v[i].first, y = v[i].second;
-			if (land[x][y] != 0)
+			if (c[x][y] == 0)
 			{
-				for (int a = -1; a < 2; a++)
-				{
-					if (y + a < 0)continue;
-					if (y + a >= w)continue;
-					for (int b = -1; b < 2; b++)
-					{
-						if (x + b < 0)continue;
-						if (x + b >= h)continue;
-
-						if (land[x + b][y + a] != 0)
-						{
-							if (land[x + b][y + a] < land[x][y])
-								land[x][y] = land[x + b][y + a];
-						}
-					}
-				}
+				dfs(x, y);
+				c_index++;
 
 			}
-		}
-		int max = 0;
-		for (int i = 0; i < h; i++)
-		{
-			for (int j = 0; j < w; j++)
-			{
-				if (max < land[i][j])
-					max = land[i][j];
-			}
-		}
-		printf("%d\n",max);
 
 
+		}
+		printf("%d\n", c_index - 1);
 	}
 }
