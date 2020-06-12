0d5664235ee3ba39e5a2fad3d54db23d12f5c6f5
haeunyoung <44631215+haeunyoung@users.noreply.github.com>
2020-06-12 13:26:28 +0900

    algorithm study

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
+#include<stdio.h>
 
-using namespace std;;
-int main()
+int node = 10;
+int data[] = { 1,10,5,8,7,6,4,3,2,9 };
+
+void show()
+{
+	int i;
+	for (i = 0; i < node; i++)
+		printf("%d ", data[i]);
+}
+
+int main(void)
 {
 
 }
